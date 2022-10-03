72992d4b2d08f436544518bd4bb944d6a1e3ac29
tae100k <tkim3@oswego.edu>
2022-10-03 23:07:49 +0900

    feat: error, isError added

--- react-query-demo/src/components/RQSuperHeroes.page.js
+++ react-query-demo/src/components/RQSuperHeroes.page.js
@@ -1,24 +1,37 @@
 //1. import hook from react-query
-import { useQuery } from "react-query"; // all data fetching hook
 import axios from "axios";
+import { useEffect } from "react";
+import { useQuery } from "react-query"; // all data fetching hook
 
 // results is an object that includes all the info that i've ever need
 // so you have to distructure.
 // isLoading - loading status, data - holding the data
 // just made it into the function to avoid inline function
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 
 export const RQSuperHeroesPage = () => {
   // results is an object that includes all the info that i've ever need
   // so you have to distructure.
   // isLoading - loading status, data - holding the data
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes); // at least 2 argument (unikey to identify this query)
+  // isError - error status
+  // error- error thrown from axios
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes
+  ); // at least 2 argument (unikey to identify this query)
   // second argument : call back func that returns a promise (make a get request to server)
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  // after a delay we see a error message,
+  // it's because react-query retrys api fail
+  if (isError) {
+    // error itself is just object so just display error.message
+    return <h2>{error.message}</h2>;
   }
 
   return (
--- react-query-demo/src/components/SuperHeroes.page.js
+++ react-query-demo/src/components/SuperHeroes.page.js
@@ -4,18 +4,29 @@ import axios from "axios";
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState();
   const [data, setData] = useState([]);
+  const [error, setError] = useState(""); // add state to track error
 
   // classic way to do it
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data); // subkcal도 async 안 쓰고 이렇게 하면 될 듯
-      setIsLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data); // subkcal도 async 안 쓰고 이렇게 하면 될 듯
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false); // classic way to do it
+      });
   }, []);
 
   // 함수가 JSX 리턴하는 거 왜 나 자주 안쓰지..?
   if (isLoading) {
     return <h3>Loading..</h3>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
